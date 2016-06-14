0a7139aabe6a08d40ffba6ef6b0fa803c5b94046
杜晓星 <1255322700@qq.com>
2016-06-14 15:35:31 +0800

    python 2.x学习 版本 MacOX 10.11.5 Python2.7.10

---
+++ Python2.x/HelloPython.py
@@ -0,0 +1,31 @@
+
+# 廖雪峰博客地址:http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000
+
+# encoding: utf-8
+
+# print "Hello Python"
+
+
+# print 'The quick brown fox','jumps over','the lazy dog'
+
+
+# print 100+200
+# print '100+200'
+
+# name = raw_input()
+# # print'输入的名字是',name
+# print'Hello', name
+
+# print absolute value of an integer:
+# a = 100
+# if a >= 0:
+#     print a
+# else:
+#     print -a
+
+a = input()
+print '仅支持输入数字', '用户输入是', a
+if a >= 0:
+    print a
+else:
+    print -a
