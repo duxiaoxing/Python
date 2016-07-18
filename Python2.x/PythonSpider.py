f6613820808514f39c22623fa78aa571dd90a709
杜晓星 <1255322700@qq.com>
2016-07-18 19:35:48 +0800

    糗事百科 爬虫

--- Python2.x/PythonSpider.py
+++ Python2.x/PythonSpider.py
@@ -84,20 +84,115 @@
 # # 保存cookie到文件
 # cookie.save(ignore_discard=True, ignore_expires=True)
 
-# 读取cookie
-import cookielib
+# # 读取cookie
+# import cookielib
+# import urllib2
+
+# # 创建MozillaCookieJar实例对象
+# cookie = cookielib.MozillaCookieJar()
+# # 从文件中读取cookie内容到变量
+# cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+# # 创建请求的request
+# req = urllib2.Request("http://www.baidu.com")
+# # 利用urllib2的build_opener方法创建一个opener
+# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+# response = opener.open(req)
+# print response.read()
+
+# 爬糗事百科帖子
+# # coding:utf-8 
+# import urllib
+# import urllib2
+# import re
+
+# url = 'http://www.qiushibaike.com/'
+# user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+# headers = { 'User-Agent' : user_agent }
+# try:
+#     request = urllib2.Request(url,headers = headers)
+#     response = urllib2.urlopen(request)
+#     # print response.read()
+# except urllib2.URLError, e:
+#     if hasattr(e,"code"):
+#         print e.code
+#     if hasattr(e,"reason"):
+#         print e.reason  
+# content = response.read().decode('utf-8')
+# pattern = re.compile('<div.*?class="article block untagged mb15>',re.S)
+# items = re.findall(pattern,content)
+# for item in items:
+#     print item[0],item[1],item[2],item[3],item[4]  
+
+
+# coding=utf-8
+import urllib
 import urllib2
+import re
+import thread
+import time
 
-# 创建MozillaCookieJar实例对象
-cookie = cookielib.MozillaCookieJar()
-# 从文件中读取cookie内容到变量
-cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
-# 创建请求的request
-req = urllib2.Request("http://www.baidu.com")
-# 利用urllib2的build_opener方法创建一个opener
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-response = opener.open(req)
-print response.read()
+class QSBK:
 
+    def __init__(self):
+        self.pageIndex = 1
+        self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        self.headers = {'User-Agent' :self.user_agent}
+        self.stories = []
+        self.enable = False
 
+    def getPage(self,pageIndex):
+        try:
+            url = 'http://www.qiushibaike.com/hot/page/' + str(pageIndex)
+            request = urllib2.Request(url,headers=self.headers)
+            response = urllib2.urlopen(request)
+            pageCode = response.read().decode('utf-8')
+            return pageCode
+        except urllib2.URLError,e:
+            if hasattr(e,"reason"):
+                print "error",e.reason
+                return None
+
+    def getPageItems(self,pageIndex):
+        pageCode = self.getPage(pageIndex)
+        if not pageCode:
+            print "page load error"
+            return None
+        pattern = re.compile('<h2>(.*?)</h2.*?content">(.*?)</.*?number">(.*?)</',re.S)
+        items = re.findall(pattern,pageCode)
+        pageStories = []
+        for item in items:
+            pageStories.append([item[0].strip(),item[1].strip(),item[2].strip()])
+        return pageStories
+
+    def loadPage(self):
+        if self.enable==True:
+            if len(self.stories)<2:
+                pageStories = self.getPageItems(self.pageIndex)
+                if pageStories:
+                    self.stories.append(pageStories)
+                    self.pageIndex +=1
+
+    def getOneStory(self,pageStories,page):
+        for story in pageStories:
+            input = raw_input()
+            self.loadPage()
+            if input == "Q":
+                self.enable = False
+                return
+            print u"第%d页\t发布人：%s\t 赞：%s\n%s" %(page,story[0],story[2],story[1])
+
+    def start(self):
+        print u'正在读取，回车查看，Q退出'
+        self.enable = True
+        self.loadPage()
+        nowPage = 0
+        while self.enable:
+            if len(self.stories)>0:
+                pageStories = self.stories[0]
+                nowPage +=1
+                del self.stories[0]
+                self.getOneStory(pageStories,nowPage)
+
+spider = QSBK()
+spider.start()
 
